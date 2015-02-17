a725bf130c724696947c262676535b8555219cff
Paola Garcia <garciaamezola@gmail.com>
2015-02-17 08:42:45 +0000

    this was my attempt at trying to get the names and dates for the seminars, however i was unsuccesful because ,my ipython kept giving me a message saying [ 'NoneType' object has no attribute 'find_all'] which i could not figure out.

--- eeb_website.py
+++ eeb_website.py
@@ -1,1 +1,13 @@
 curl 'https://www.eeb.ucla.edu/seminars.php?id=[001-800]' > 1.html
+history
+from bs4 import BeautifulSoup
+soup = BeautifulSoup(open("eeb.html"))
+seminar_pane = soup.find(id="secondary-nav")
+dates = seminar_pane.find_all("p", class_="subnavlinks")
+dates_p = seminar_pane.find_all("p", class_="subnavlinks")
+for p in dates_p:
+        div = p.find("div", class_="subnavlinks")
+        seminar_dates = div.span
+        print seminar_dates.string
+history
+
